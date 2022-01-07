00b26264308a7ccc23581b68f6cbcb255624db95
kieuhuan <kieuhuanbka@gmail.com>
2022-01-07 11:21:33 +0700

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	Rectangle rectangle01=new Rectangle(10,5);
+        System.out.println(rectangle01);
+    }
+
+
+}
+
